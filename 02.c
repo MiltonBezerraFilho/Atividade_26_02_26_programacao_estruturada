80a9b000ecaef29d28e003cf2b3c3a6241205a0b
Milton Bezerra do Vale Filho <miltonbfilho206@gmail.com>
2026-03-03 09:30:46 -0300

    base for 02.c

---
+++ 02.c
@@ -0,0 +1,13 @@
+/*Em um programa, crie uma matriz 4×4 e preencha-a com valores inteiros (os dados podem
+ser definidos diretamente no código). Com essa matriz, calcule:
+• A soma da diagonal principal;
+• A soma da diagonal secundária.
+Ao final, informe se as duas somas são iguais ou diferentes.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
