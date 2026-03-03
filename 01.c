fdb64fd5168c653e03cf417b770f654dd60f4756
Milton Bezerra do Vale Filho <miltonbfilho206@gmail.com>
2026-03-03 08:09:18 -0300

    structure for 01.c

---
+++ 01.c
@@ -0,0 +1,20 @@
+/*Observação:
+Para todos os programas que requeiram o uso de arrays, recomenda-se a implementação de duas
+versões:
+• Uma versão em que o número de elementos (N) seja definido diretamente no código. Nesse
+caso, a quantidade de valores a serem inseridos deve ser informada ao usuário na tela (por
+exemplo: “Digite 5 números inteiros”);
+• Uma versão em que o número de elementos (N) seja informado pelo usuário. Nesse caso,
+o tamanho do array deve ser definido dinamicamente.*/
+
+/*Implemente um programa que leia N números inteiros. Armazene os valores em um array.
+Determine os menores e maiores valores armazenados, calcule a média entre o menor
+e o maior valor e determine as posições (índices) em que esses valores ocorrem.
+Ao final, imprima todas as informações calculadas.*/
+
+#include <stdio.h>
+
+int main() {
+
+    return 0;
+}
