e9ea1ee82e3090558c386dbc44e3263b5855d7f9
Milton Bezerra do Vale Filho <miltonbfilho206@gmail.com>
2026-03-03 14:02:59 -0300

    halfway through 02.c

--- 02.c
+++ 02.c
@@ -8,6 +8,26 @@ Ao final, informe se as duas somas são iguais ou diferentes.*/
 #include <stdlib.h>
 #define TAMANHO 4
 
+void analizarMatriz (int matriz[TAMANHO][TAMANHO]){
+    int somaPrincipal = 0, somaSecundaria = 0;
+
+    for(int i = 0; i < TAMANHO; i++){
+        for(int j = 0; j < TAMANHO; j++){
+            if (i == j) {
+                somaPrincipal += matriz[i][j];
+            } if (i + j == TAMANHO - 1){
+                somaSecundaria += matriz[i][j];
+            }
+        }
+    }
+    printf("A soma principal eh %d\n", somaPrincipal);
+    printf("A soma secundaria eh %d\n", somaSecundaria);
+    if (somaPrincipal == somaSecundaria) {
+        printf("A soma principal e a secundaria sao iguais.\n");
+    } else {
+        printf("A soma princiapal e a secundaria sao diferentes.\n");
+    }
+}
 
 int main() {
 
