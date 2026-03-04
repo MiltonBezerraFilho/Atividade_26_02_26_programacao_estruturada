fa341c45c59ac140c05bbfe17a29bef7f9058566
Milton Bezerra do Vale Filho <miltonbfilho206@gmail.com>
2026-03-04 09:18:29 -0300

    add 05.c

---
+++ 05.c
@@ -0,0 +1,15 @@
+/*Utilizando a mesma lógica da questão anterior, implemente um programa que leia N alunos
+com sua respectiva nota (utilize struct Aluno para armazenar os dados dos alunos —
+nome, nota, situação) e armazene os dados em um vetor de structs. Calcule:
+• Percentual de aprovados;
+• Percentual de reprovados;
+• Média da turma, utilizando uma função que receba o vetor como parâmetro.
+Ao final, imprima um relatório com as informações calculadas.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
