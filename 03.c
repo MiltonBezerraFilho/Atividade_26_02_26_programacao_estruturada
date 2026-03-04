4c049ae8a14b2d82a05e4e6cdb588d88db5a8aeb
Milton Bezerra do Vale Filho <miltonbfilho206@gmail.com>
2026-03-04 08:09:17 -0300

    added 03.c

---
+++ 03.c
@@ -0,0 +1,15 @@
+/*Implemente um sistema que cadastre um número N de alunos. Para cada aluno, armazene:
+Matrícula, Nome e Nota. O programa deve:
+• Permitir o cadastro;
+• Listar todos os alunos;
+• Informar o aluno com a maior nota.
+As informações devem ser armazenadas em um vetor de struct Aluno. A impressão deve
+ser feita por meio de uma função.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
