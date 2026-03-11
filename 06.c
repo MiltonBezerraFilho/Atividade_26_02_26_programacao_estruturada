0bb97c02179e87ca84f8fcabd84d169b6130a7d9
Milton Bezerra do Vale Filho <miltonbfilho206@gmail.com>
2026-03-11 08:13:03 -0300

    add 06.c

---
+++ 06.c
@@ -0,0 +1,14 @@
+/*Implemente um sistema que armazene produtos com: Código, Nome, Categoria e Preço.
+As categorias possíveis são ALIMENTO, BEBIDA e LIMPEZA. Defina um enum para
+representar a categoria e utilize uma struct para armazenar os dados. O programa deve:
+• Cadastrar uma quantidade N de produtos (informado pelo usuário);
+• Listar os produtos por categoria;
+• Calcular o valor total dos produtos cadastrados.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    return 0;
+}
